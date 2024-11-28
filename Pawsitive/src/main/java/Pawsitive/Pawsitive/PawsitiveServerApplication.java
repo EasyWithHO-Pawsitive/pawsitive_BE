dc8af441ca7647160a27675e0c74a90000c57921
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-28 13:04:51 +0900

    init: 프로젝트 세팅

---
+++ Pawsitive/src/main/java/Pawsitive/Pawsitive/PawsitiveServerApplication.java
@@ -0,0 +1,13 @@
+package Pawsitive.Pawsitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsitiveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsitiveServerApplication.class, args);
+	}
+
+}
---
+++ Pawsitive/src/test/java/Pawsitive/Pawsitive/PawsitiveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Pawsitive.Pawsitive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsitiveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
