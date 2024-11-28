0d94958516939e96a4019a10d10ef415d6823441
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-28 20:11:07 +0900

    Initial commit

---
+++ src/main/java/pawsitive/pawsitive_BE/PawsitiveBeApplication.java
@@ -0,0 +1,13 @@
+package pawsitive.pawsitive_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsitiveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsitiveBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pawsitive/pawsitive_BE/PawsitiveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pawsitive.pawsitive_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsitiveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
